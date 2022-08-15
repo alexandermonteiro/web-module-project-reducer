71a00ac023ef5ee2942dd8d8b6707f14fa9777d7
Alexander Monteiro <alexandermonteiro@Priscilas-MBP.attlocal.net>
2022-08-15 12:45:20 -0700

    add addone

--- src/components/App.js
+++ src/components/App.js
@@ -7,8 +7,13 @@ import CalcButton from "./CalcButton";
 
 import reducer, { initialState } from "./../reducers";
 
+import { addOne } from "../actions";
+
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const handle1Click = () => {
+    dispatch(addOne());
+  };
 
   return (
     <div className="App">
@@ -39,7 +44,7 @@ function App() {
             </div>
 
             <div className="row">
-              <CalcButton value={1} />
+              <CalcButton value={1} onClick={handle1Click} />
               <CalcButton value={2} />
               <CalcButton value={3} />
             </div>
